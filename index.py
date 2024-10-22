420cd707a673922f20cf30dacf0383ca6ca9cd3f
Manoel Ferreira Matos <95552207+ManelFer@users.noreply.github.com>
2024-10-22 15:17:45 -0300

    alteração

--- index.py
+++ index.py
@@ -1,5 +1,8 @@
-import seaborn as sns
-import matplotlib.pyplot as plt
+import dash
+import dash_core_components as dcc
+import dash_html_components as html
+from dash.dependencies import Input, Output
+import plotly.express as px
 import pandas as pd
 
 # Carregar os dados
@@ -7,19 +10,42 @@ data_pas = pd.read_csv("https://gist.githubusercontent.com/adolfoguimaraes/7202a
                         names=['ano','sigla_uf','nome_uf','id_municipio','nome_municipio','nome_regiao','nome_empresa','porte_empresa','tecnologia','transmissao','acessos'], 
                         low_memory=False)
 
-# Visualização
-fig, axs = plt.subplots(ncols=2, nrows=2, figsize=(10,10))
+# Gráfico de pizza para estados
+estado_acessos = data_pas.groupby('nome_uf')['acessos'].sum().reset_index()
+fig_estado = px.pie(estado_acessos, values='acessos', names='nome_uf', title='Acessos por Estado')
 
-# Exemplo de boxplot com colunas existentes
-sns.boxplot(data=data_pas, x="porte_empresa", y="acessos", ax=axs[0][0])
-axs[0][0].set_title('Acessos por Porte da Empresa')
+# Gráfico de pizza para empresas
+empresa_acessos = data_pas.groupby('nome_empresa')['acessos'].sum().reset_index()
+fig_empresa = px.pie(empresa_acessos, values='acessos', names='nome_empresa', title='Acessos por Empresa')
 
-sns.boxplot(data=data_pas, x="tecnologia", y="acessos", ax=axs[0][1])
-axs[0][1].set_title('Acessos por Tecnologia')
+# Gráfico de pizza para tecnologia
+tecnologia_acessos = data_pas.groupby('tecnologia')['acessos'].sum().reset_index()
+fig_tecnologia = px.pie(tecnologia_acessos, values='acessos', names='tecnologia', title='Acessos por Tecnologia')
 
-# Adicione mais visualizações conforme necessário
-sns.boxplot(data=data_pas, x="nome_uf", y="acesso", ax=axs[1][0])
-axs[1][0].set_title('Acessos por regiao')
+# Gráfico de pizza para acessos totais
+acessos_totais = data_pas['acessos'].sum()
+fig_acessos = px.pie(data_pas.groupby('nome_uf')['acessos'].sum().reset_index(), values='acessos', names='nome_uf', title='Acessos Totais')
 
-plt.tight_layout()
-plt.show()
+# Criar o dashboard
+app = dash.Dash(__name__)
+
+app.layout = html.Div([
+    html.H1('Dashboard de Acessos'),
+    dcc.Tabs([
+        dcc.Tab(label ='Acessos por Estado', children=[
+            dcc.Graph(figure=fig_estado)
+        ]),
+        dcc.Tab(label='Acessos por Empresa', children=[
+            dcc.Graph(figure=fig_empresa)
+        ]),
+        dcc.Tab(label='Acessos por Tecnologia', children=[
+            dcc.Graph(figure=fig_tecnologia)
+        ]),
+        dcc.Tab(label='Acessos Totais', children=[
+            dcc.Graph(figure=fig_acessos)
+        ])
+    ])
+])
+
+if __name__ == '__main__':
+    app.run_server()
